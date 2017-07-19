cdd7e706fda1abc07d40b7929407f9dc3eca2da5
abgandhi <abgandhi@cisco.com>
2017-07-19 16:03:29 -0700

    Minor change in install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -xe
 
-dotfiles = ".bashrc_abgandhi .vim .vimrc .tmux.conf"
+dotfiles=".bashrc_abgandhi .vim .vimrc .tmux.conf"
 
 echo "Creating symlink"
-for file in dotfiles; do
+for file in $dotfiles; do
         ln -snvf $file ~/.$file
 done
 
