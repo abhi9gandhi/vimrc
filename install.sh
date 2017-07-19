1f16a4e1e24604e7fc5b27d01f33da132b6cb056
abgandhi <abgandhi@cisco.com>
2017-07-19 16:26:15 -0700

    Minor change in install script

--- install.sh
+++ install.sh
@@ -7,6 +7,13 @@ for file in $dotfiles; do
         ln -snvf $file ~/.$file
 done
 
+# Add bash aliases.
+if [ -f ~/.bashrc_abgandhi ]; then
+    source ~/.bashrc_abgandhi
+else
+
+fi
+
 echo "Done."
 
 
