15e52264795d716602309faf6b059c3cfec0fa10
abgandhi <abgandhi@cisco.com>
2017-07-20 23:17:49 -0700

    Added dependencies to install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 dotfiles=".bashrc_abgandhi .vim .vimrc .tmux.conf"
 
+echo "Downloading dependencies"
+sudo apt-get install vim -y
+sudo apt-get install tmux -y
+
 echo "Creating symlink"
 for file in $dotfiles; do
         ln -snvf $file ~/$file
