03b7f6bbfaa129c84c7ce828aca13335bd68be16
abgandhi <abgandhi@cisco.com>
2017-07-20 23:04:41 -0700

    Edited install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,11 @@ dotfiles=".bashrc_abgandhi .vim .vimrc .tmux.conf"
 
 echo "Creating symlink"
 for file in $dotfiles; do
-        ln -snvf $file ~/.$file
+        ln -snvf $file ~/$file
 done
 
 # Add bash aliases.
-if [ -f ~/.bashrc_abgandhi ]; then
-    source ~/.bashrc_abgandhi
-fi
+source ~/.bashrc_abgandhi
 
 echo "Done."
 
